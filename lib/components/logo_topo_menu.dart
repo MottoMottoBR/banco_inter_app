7ad6776082c97d54987ed05be60f9108f891820f
MottoMottoBR <alessandro.aoc@gmail.com>
2024-04-08 15:56:18 -0400

    Removido o Logo Inter e Menus do Top para um arquivo separado

---
+++ lib/components/logo_topo_menu.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../styles/app_cores.dart';
+
+class logoTopoMenu extends StatelessWidget {
+  const logoTopoMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const double tamanhoFixoIcons = 25;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Image.asset(
+          'lib/img/inter_logo.png',
+          width: 110,
+          height: 110,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 50,
+                height: 50,
+                child: Icon(
+                  Icons.visibility_outlined,
+                  size: tamanhoFixoIcons,
+                  color: AppCores.CinzaBotaoUser,
+                ),
+              ),
+              const SizedBox(
+                width: 50,
+                height: 50,
+                child: Icon(
+                  Icons.search,
+                  size: tamanhoFixoIcons,
+                  color: AppCores.CinzaBotaoUser,
+                ),
+              ),
+              Container(
+                width: 35,
+                height: 35,
+                decoration: const BoxDecoration(
+                  color: AppCores.CinzaBotaoUser,
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.person,
+                  size: tamanhoFixoIcons,
+                  color: AppCores.BrancoCards,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/paginas/my_home_page.dart
+++ lib/paginas/my_home_page.dart
@@ -3,67 +3,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import '../components/logo_topo_menu.dart';
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const double tamanhoFixoIcons = 25;
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: AppCores.CinzaBackground,
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(8.0),
         child: Column(
           children: [
             // Logo Inter e botões do top Principal
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset(
-                  'lib/img/inter_logo.png',
-                  width: 120,
-                  height: 120,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 10),
-                  child: Row(
-                    children: [
-                      const SizedBox(
-                        width: 50,
-                        height: 50,
-                        child: Icon(
-                          Icons.visibility_outlined,
-                          size: tamanhoFixoIcons,
-                          color: AppCores.CinzaBotaoUser,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 50,
-                        height: 50,
-                        child: Icon(
-                          Icons.search,
-                          size: tamanhoFixoIcons,
-                          color: AppCores.CinzaBotaoUser,
-                        ),
-                      ),
-                      Container(
-                        width: 35,
-                        height: 35,
-                        decoration: const BoxDecoration(
-                          color: AppCores.CinzaBotaoUser,
-                          shape: BoxShape.circle,
-                        ),
-                        child: const Icon(
-                          Icons.person,
-                          size: 25,
-                          color: AppCores.BrancoCards,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            )
+            logoTopoMenu()
           ],
         ),
       ),
